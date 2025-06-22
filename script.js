3e180a1a43356f9ba2f1e92f8674b67d919d80d6
Ayushm010 <mohapatraayush587@gmail.com>
2025-06-22 10:45:38 +0530

    feat: add hover effect to change square color on mouse enter

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const container = document.querySelector(".container");
 function createGrid(size) {
-    
-    let squareSize = 630 / size; //one div size = total size of the container divided by the total number of squares in the div
-    
+  //one div size = total size of the container divided by the total number of squares in the div
+  let squareSize = 630 / size;
 
-    for (let i = 0; i < size * size; i++) {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.style.cssText = `border: 1px solid black; height:${squareSize}px ; width: ${squareSize}px`;
-        container.appendChild(card);
-        
-        
-    }
+  //for clearing the child nodes/elements
+  container.replaceChildren();
+
+  for (let i = 0; i < size * size; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.style.cssText = `border: 1px solid black; height:${squareSize}px ; width: ${squareSize}px`;
+
+
+    //Adding the hover effect
+    card.addEventListener("mouseenter", () => {
+      card.style.backgroundColor = "purple";
+    });
+
+    container.appendChild(card);//adding the card div to the container
+  }
 }
 
+//gets the input from the fuser
+function setupResetButton() {
 
-let btn = document.querySelector('#btn');
+  let btn = document.querySelector('#btn');
 
-btn.addEventListener('click',(event) => {
-   let size;
-   while(!(size >= 1 && size <= 100)){
-     size = prompt("Enter a num between 1-100");
-   }
-   createGrid(size);
-});
+  btn.addEventListener('click', (event) => {
+    let size;
+    while (!(size >= 1 && size <= 100)) {
+      size = parseInt(prompt("Enter a num between 1–100"));
+    }
+    createGrid(size);
+  });
+}
+
+createGrid(16);//default grid
+setupResetButton();
